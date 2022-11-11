69daffba3c9c3657e71a66c89ad12ba70ec9517d
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-11-10 20:56:20 -0500

    Created a new project

---
+++ P7_UrbanRead/P7_UrbanRead/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P7_UrbanRead // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
