9228caee68bdc6d98a77e0a069296716e91e3e9a
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-11-12 16:36:51 -0500

    Book Class - Work in progress

---
+++ P7_UrbanRead/P7_UrbanRead/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P7_UrbanRead
+{
+    internal class Book
+    {
+        private string _Cover; //Source of the cover image
+        private string _Title;
+        private string _Description;
+        private int _TotalPages;
+        private DateTime _Edition;
+        private int _ISBN;
+        private Author _Author;
+        private Publisher _Publisher;
+        private List<string> _Language = new List<string> {"Portuguese", "English", "Spanish", "French", "Italian"};
+        private List<string> _Genre = new List<string> {"Fiction", "Non-Fiction"};
+        private List<string> _SubGenre = new List<string> { };
+
+        public List<string> Language 
+        {
+            get { return _Language; }
+        }
+
+        private enum Type
+        {
+
+        }
+        private enum EbookFormart
+        {
+
+        }
+
+    }
+}
