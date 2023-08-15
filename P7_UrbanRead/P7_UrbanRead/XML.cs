f471563a14bf96592e6153ecd3bfd81a17cd69c8
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2023-08-15 07:27:53 -0400

    Issue #19 - Created XML class and ExportDB()

---
+++ P7_UrbanRead/P7_UrbanRead/XML.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace P7_UrbanRead
+{
+    internal class XML
+    {
+        private static string _USERNAME = Environment.UserName;
+        private static string _PATH = @$"C:\Users\{_USERNAME}\AppData\Local\Temp";
+
+        public static void ExportDB(List<Book> library)
+        {
+            XmlSerializer seria = new XmlSerializer(typeof(List<Book>));
+            using (StreamWriter writer = new StreamWriter(_PATH));
+            {
+                seria.Serialize(writer, library);
+            }
+
+        }
+
+    }
+}
