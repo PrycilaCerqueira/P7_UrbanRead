ea8457fd7b7531513c33147c313785eebfd1a6e2
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2023-08-21 07:20:51 -0400

    Issue #19 - Implemented Folder creation as well as XML file export

--- P7_UrbanRead/P7_UrbanRead/XML.cs
+++ P7_UrbanRead/P7_UrbanRead/XML.cs
@@ -2,20 +2,35 @@
 using System.Xml.Serialization;
 using System.IO;
 using System.Collections.Generic;
-
+using System.Reflection.Metadata.Ecma335;
+using System.Linq.Expressions;
 
 namespace P7_UrbanRead
 {
     internal class XML
     {
         static string USERNAME = Environment.UserName;
-        static string PATH = @$"C:\Users\{USERNAME}\Documents\UrbanRead_Library";
-
-        public static void ExportFile(List<Book> library)
+        static string DIRECTORY = @$"C:\Users\{USERNAME}\Documents\UrbanRead";
+        static string FILE = System.IO.Path.Combine(DIRECTORY, "Book_Library.xml");
+        
+       public static void ExportFile(List<Book> library)
         {
 
+            try 
+            {
+                //Creates the UrbanRead folder in the given location if it doesn't exit
+                if (!Directory.Exists(DIRECTORY))
+                {
+                    Directory.CreateDirectory(DIRECTORY);
+                }
+            }
+            catch (Exception)  
+            {
+                //If the folder already exists in the given location, it fails silently
+            }
+
             XmlSerializer ser = new XmlSerializer(typeof(List<Book>));
-            using (StreamWriter writer = new StreamWriter(PATH))
+            using (StreamWriter writer = new StreamWriter(FILE))
             {
                 ser.Serialize(writer, library);
             }
