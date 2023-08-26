144eae844da44ee86d13811e61afc735cef334ca
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2023-08-26 11:30:18 -0400

    Issue #19 - Created XML.CreateRepoFolde() and updated XML.ExportFile()

--- P7_UrbanRead/P7_UrbanRead/XML.cs
+++ P7_UrbanRead/P7_UrbanRead/XML.cs
@@ -10,28 +10,38 @@ namespace P7_UrbanRead
     internal class XML
     {
         static string USERNAME = Environment.UserName;
-        static string DIRECTORY = @$"C:\Users\{USERNAME}\Documents\UrbanRead";
-        static string FILE = System.IO.Path.Combine(DIRECTORY, "Book_Library.xml");
+        static string FOLDER = @$"C:\Users\{USERNAME}\Documents\UrbanRead";
+        static string FILE = System.IO.Path.Combine(FOLDER, "Book_Library.xml");
         
+
+       
+        /// <summary>
+        /// Create the XML repository folder on the user's PC profile
+        /// </summary>
+        public static void CreateRepoFolder()
+        {
+            try
+            {
+                //Creates the UrbanRead folder in the given location if it doesn't exit
+                if (!Directory.Exists(FOLDER))
+                {
+                    Directory.CreateDirectory(FOLDER);
+                }
+            }
+            catch (Exception)
+            {
+                //If the folder already exists in the given location, it fails silently
+            }
+
+        }
+
         /// <summary>
         /// Serializer the Book Library into XML File
         /// </summary>
         /// <param name="library">Book Library</param>
-       public static void ExportFile(List<Book> library)
-       {
-
-            try 
-            {
-                //Creates the UrbanRead folder in the given location if it doesn't exit
-                if (!Directory.Exists(DIRECTORY))
-                {
-                    Directory.CreateDirectory(DIRECTORY);
-                }
-            }
-            catch (Exception)  
-            {
-                //If the folder already exists in the given location, it fails silently
-            }
+        public static void ExportFile(List<Book> library)
+        { 
+            CreateRepoFolder();
 
             //Exports the file in XML format into the Directory 
             XmlSerializer ser = new XmlSerializer(typeof(List<Book>));
@@ -40,7 +50,7 @@ namespace P7_UrbanRead
                 ser.Serialize(writer, library);
             }
 
-       }
+        }
 
         /// <summary>
         ///  Deserializer the XML File into the Book Library
