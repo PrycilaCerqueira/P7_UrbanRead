beb27bbeb75f9b605add527b99b96ffca9583dfa
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2023-08-28 06:30:31 -0400

    Issue #21 - Structuring the Main() for the Search FLOW

--- P7_UrbanRead/P7_UrbanRead/Program.cs
+++ P7_UrbanRead/P7_UrbanRead/Program.cs
@@ -5,25 +5,22 @@
         static void Main(string[] args)
         {
 
-           
-            
             //Users' input their search parameters
             List<string> searchParameters= UI.BookSearchMenu();
 
-            //Establishes the API connection with Google Books to retrieve the books data 
-            GoogleBooksJson.Root GBCollection = Help.GoogleBookAPIConnector(searchParameters);
-            List<Book> library = Help.LoadGoogleBooksData(GBCollection);
-            
-            //Save the library file into the users' profile folder for a fast load
-            XML.ExportFile(library);
-            XML.ImportFile();
-            library = XML.LoadLocalLibrary();
-
+            //Loads the Local Library if file exists 
+            List<Book> library = XML.LoadLocalLibrary();
             if (library == null)
             {
-
+                //Establishes the API connection with Google Books to retrieve the books data 
+                GoogleBooksJson.Root GBCollection = Help.GoogleBookAPIConnector(searchParameters);
+                library = Help.LoadGoogleBooksData(GBCollection);
             }
-
+                        
+            //Save the library file into the users' profile folder for a fast load
+            XML.ExportFile(library);
+            
+            
             // Set books reading status 
             //FuncHelp.SetReadingStatus(locBook);
 
