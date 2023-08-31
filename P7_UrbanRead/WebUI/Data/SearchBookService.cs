4b1780295ec944bc7a986a36521d4556df48d86c
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2023-08-30 22:09:03 -0400

    WebUI - Recreated the SearchBookService.cs and added 3 search methods to develop

---
+++ P7_UrbanRead/WebUI/Data/SearchBookService.cs
@@ -0,0 +1,20 @@
+﻿namespace WebUI.Data
+{
+    public class SearchBookService
+    {
+        public static void SearchOnGoogleBooks()
+        {
+
+        }
+
+        public static void SearchOnLocalLibrary()
+        {
+
+        }
+
+        public static void SearchOnUserBookshelf()
+        {
+
+        }
+    }
+}
