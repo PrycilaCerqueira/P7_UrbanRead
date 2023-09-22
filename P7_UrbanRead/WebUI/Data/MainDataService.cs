f0afdf65670d08fe94732c309bb86fd306421261
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2023-09-22 05:58:27 -0400

    Issue #15 - Search output first draft

--- P7_UrbanRead/WebUI/Data/MainDataService.cs
+++ P7_UrbanRead/WebUI/Data/MainDataService.cs
@@ -5,17 +5,10 @@ namespace WebUI.Data
 {
     public class MainDataService
     {      
-        public List<string> SearchBook()
+        public static List<Book> SearchBook()
         {
             List<Book> searchResults = Help.initiateBookSearch();
-
-            List <string> searchResultsToString = new List<string>();
-            foreach (Book result in searchResults)
-            {
-                searchResultsToString.Add(result.ToString());
-            }
-            return searchResultsToString;
-
+            return searchResults;
         }
     }
 }
