b28533aa46d2cfc430defdee1c062bda52528813
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2023-11-01 12:04:58 -0400

    Updated the ISBN.cs to accomodate the changes for issue #25

--- P7_UrbanRead/P7_UrbanRead/ISBN.cs
+++ P7_UrbanRead/P7_UrbanRead/ISBN.cs
@@ -5,21 +5,14 @@ namespace P7_UrbanRead
     public class ISBN
     {
         private long _Isbn;
-        //private long _Isbn13;
 
         public long Isbn
         {
             get { return _Isbn; }
             set { _Isbn = value; }
         }
-        /*public long Isbn13
-        {
-            get { return _Isbn13; }
-            set { _Isbn13 = value; }
-        }
-        */
-
-     
+        
+        
         public override string ToString()
         {
             return $"{Isbn}";
