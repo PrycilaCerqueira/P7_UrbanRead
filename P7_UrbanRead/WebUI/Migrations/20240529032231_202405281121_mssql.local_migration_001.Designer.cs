c7d0ac4fb0e401acf92371f9f6eb1b24e254e115
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2024-05-29 23:37:36 -0400

    Issue #43 - Recreated Details page as DetailsV2. Compiler working now.

--- P7_UrbanRead/WebUI/Migrations/20240529032231_202405281121_mssql.local_migration_001.Designer.cs
+++ P7_UrbanRead/WebUI/Migrations/20240529032231_202405281121_mssql.local_migration_001.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebUI.Data;
 
@@ -11,9 +12,11 @@ using WebUI.Data;
 namespace WebUI.Migrations
 {
     [DbContext(typeof(WebUIContext))]
-    partial class WebUIContextModelSnapshot : ModelSnapshot
+    [Migration("20240529032231_202405281121_mssql.local_migration_001")]
+    partial class _202405281121_mssqllocal_migration_001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
